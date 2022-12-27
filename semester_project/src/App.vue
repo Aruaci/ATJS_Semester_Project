cf7eed42c3d39beb9a08e0b76dccfeb207f024c2
Aruaci <laura.cesar3@gmail.comm>
2022-12-27 08:01:20 +0100

    first styling attempts, created headline, card component, border ornaments and added test background

--- semester_project/src/App.vue
+++ semester_project/src/App.vue
@@ -1,29 +1,55 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import SpellCard from './components/SpellCard.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <img src="./assets/corner-left.webp" alt="" class="corner-ornament" id="top-left">
+  <img src="./assets/corner-right.webp" alt="" class="corner-ornament" id="top-right">
+  <img src="./assets/corner-bottom-left.webp" alt="" class="corner-ornament" id="bottom-left">
+  <img src="./assets/corner-bottom-right.webp" alt="" class="corner-ornament" id="bottom-right">
+  <header>
+    <h1>Charms</h1>
+  </header>
+  <spell-card></spell-card>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+header{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 120px;
+  /* background-color: green; */
+  color: white;
+  font-size: 1.7em;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+h1 {
+  color: white;
+  text-shadow: 0px 2px 2px rgb(83, 93, 82);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.corner-ornament {
+  height: 150px;
+  position: absolute;
+}
+
+#top-left {
+  top: 0;
+  left: 0;
+}
+
+#top-right{
+  top: 0;
+  right: 0;
+}
+
+#bottom-left {
+  bottom: 0;
+  left: 0;
+}
+
+#bottom-right {
+  bottom: 0;
+  right: 0;
 }
 </style>
---
+++ semester_project/src/components/SpellCard.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    data() {
+        return {
+            spellName: 'Spell Name',
+            spellDescription: 'Spell description like what it does and stuff'
+        };
+    }
+}
+</script>
+
+<template>
+    <div class="spell-card">
+        <h3>{{ spellName }}</h3>
+        <p>{{ spellDescription }}</p>
+    </div>
+</template>
+
+<style>
+    .spell-card {
+        border: 2px solid rgb(171, 193, 244);
+        background-color: white;
+        border-radius: 5px;
+        display: flex;
+        flex-direction: column;
+        place-items: center;
+        height: 200px;
+        width: 150px;
+        padding: 10px;
+    }
+
+    .spell-card p {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
